f9ef369bd76f3b3a8b3bc92525d20cf1660d7268
Lucas Felipe <lucassantanafelipe@gmail.com>
2024-10-20 10:14:04 -0300

    import modules

---
+++ weatherapp.py
@@ -0,0 +1,15 @@
+import sys
+import requests
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, 
+                             QLineEdit, QPushButton, QVBoxLayout)
+from PyQt5.QtCore import Qt
+
+class WeatherApp(QWidget):
+    def __init__(self):
+        super().__init__()
+
+def main():
+    print("Yeah")
+
+if __name__ ==  "__main__":
+    main()
