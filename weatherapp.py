110ec619cccdbd89b97060452b06b8ed961253fb
Lucas Felipe <lucassantanafelipe@gmail.com>
2024-10-21 20:45:40 -0300

    adding labels

--- weatherapp.py
+++ weatherapp.py
@@ -7,7 +7,13 @@ from PyQt5.QtCore import Qt
 class WeatherApp(QWidget):
     def __init__(self):
         super().__init__()
-
+        self.city_label = QLabel("Enter city name: ", self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("Get Weather", self)
+        self.temperature_label = QLabel("70°F")
+        self.emoji_label = QLabel("☀️")
+        self.description_label = QLabel("Sunny", self)
+        
 if __name__ ==  "__main__":
      app = QApplication(sys.argv)
      weather_app = WeatherApp()
