28482551dfd5f9089f5f65086fd7534cb8a6f057
Lucas Felipe <lucassantanafelipe@gmail.com>
2024-10-29 20:04:48 -0300

    refactor: modularize temperature and weather description display

--- weatherapp.py
+++ weatherapp.py
@@ -118,7 +118,9 @@ class WeatherApp(QWidget):
     def display_error(self, message):
         self.temperature_label.setStyleSheet("font-size: 30px;")
         self.temperature_label.setText(message)
-    
+        self.emoji_label.clear()
+        self.description_label.clear()
+        
     def display_weather(self, data):
         self.temperature_label.setStyleSheet("font-size: 75px;")
         temperature_k = data["main"]["temp"]
